30843ab75d2b504d23faec024425860d5c6fe1f7
Ugur Muslim <ugur.muslim@optimum7.com>
2025-10-08 17:12:43 +0300

    new site

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ugur-resume/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
